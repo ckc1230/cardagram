98ebc36d460967485e9a098cc6ede852b1c398c7
LilyCole <Lilia.Cole@gmail.com>
2017-01-02 13:02:38 -0800

    adds views, updates package.json to start server.js and includes /models

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
+
+/* DATABASE */
+
+var db = require('./models');
+
+/* ROUTES */ 
+
+app.get('/', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 /* SERVER SET UP */ 
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Your server is running on port 3000');
-})
+});
