7e918c90c45cb98bd38689a06adaa4cc7300a5e9
ckc1230 <chriskchan1230@gmail.com>
2017-01-02 17:36:02 -0800

    WIP: adds question creation to seed file and view. creation stuck on ecard because cannot find theme

--- seed.js
+++ seed.js
@@ -1,7 +1,22 @@
 var db = require("./models");
 
+var questionList = [];
 var themeList = [];
 var eCardList = [];
+
+questionList.push({
+  prompt: "You're Awesome because _____ .",
+  response: "",
+  image: "http://www.kazzledazz.com/wp-content/uploads/2015/02/friendship-cover.jpeg",
+  theme: "Happy Birthday!"
+})
+
+questionList.push({
+  prompt: "If I could get you anything in the world, it would have to be _____ .",
+  response: "",
+  image: "http://www.kiplinger.com/slideshow/credit/T065-S001-15-gifts-that-keep-on-giving/images/intro.jpg",
+  theme: "Happy Birthday!"
+})
 
 themeList.push({
   title: "Happy Birthday!",
@@ -23,24 +38,53 @@ eCardList.push({
   theme: "Happy New Year!"
 });
 
-var savedThemes = [];
-
-db.Theme.remove({}, function(err, themes) {
-  db.Theme.create(themeList, function(err, themes) {
+db.Question.remove({}, function(err, questions) {
+  db.Question.create(questionList, function(err, questions) {
     if (err) { return console.log("Error:", err) }
-  });
-  console.log('Themes SAVED');
-  db.ECard.remove({}, function(err, ecards) {
-    eCardList.forEach(function(eCardData) {
-      var eCard = new db.ECard(eCardData);
-      db.Theme.findOne({title: eCardData.theme}, function(err, foundTheme) {
-        if (err) { return console.log("Error:",err) };
-        eCard.theme = foundTheme;
-        eCard.save(function(err, savedECard) {
-          if (err) { return console.log("Error:",err) };
+    console.log('Questions SAVED');
+  })
+    // Creating Themes
+    // db.Theme.remove({}, function(err, themes) {
+    //   db.Theme.create(themeList, function(err, themes) {
+    //     if (err) { return console.log("Error:", err) }
+    //   });
+    //   console.log('Themes SAVED');
+
+
+      db.Theme.remove({}, function(err, themes) {
+        themeList.forEach(function(themeData) {
+          var theme = new db.Theme(themeData);
+          db.Question.find({ theme: themeData.title}, function(err, foundQuestions) {
+            if (err) { return console.log("Error:",err) };
+            theme.questions = foundQuestions;
+            theme.save(function(err, savedECard) {
+              if (err) { return console.log("Error:",err) };
+            })
+          })
         })
-      })
-    })
-  });
-  console.log('ECards SAVED');
-});
+      
+        console.log('Themes SAVED');
+
+        // Creating eCards
+        db.ECard.remove({}, function(err, ecards) {
+          eCardList.forEach(function(eCardData) {
+            var eCard = new db.ECard(eCardData);
+            db.Theme.findOne({title: eCardData.theme}, function(err, foundTheme) {
+              console.log("eCardData.theme: ", eCardData.theme)
+              console.log("foundTheme: ", foundTheme)
+              if (err) { return console.log("Error:",err) };
+              eCard.theme = foundTheme;
+              eCard.save(function(err, savedECard) {
+                if (err) { return console.log("Error:",err) };
+              })
+            })
+          })
+        });
+        console.log('ECards SAVED');
+        // End eCards
+      });
+    // End Themes
+
+  // End Questions
+})
+
