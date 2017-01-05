c4341b053a6a9eae08c1c13bbc166efdf2958fb5
LilyCole <Lilia.Cole@gmail.com>
2017-01-05 11:47:17 -0800

    adds dropdown for theme and saves to ecard

--- public/scripts/controllers/ECardsIndexController.js
+++ public/scripts/controllers/ECardsIndexController.js
@@ -8,12 +8,22 @@ function ECardsIndexController($http) {
   var vm = this;
   vm.newECard = {};
   vm.ecards = [];
+  vm.themes = [];
 
   $http({
     method: 'GET',
     url: '/api/ecards'
   }).then(function successCallback(response) {
     vm.ecards = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  $http({
+    method: 'GET',
+    url: '/api/themes'
+  }).then(function successCallback(response) {
+    vm.themes = response.data;
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
