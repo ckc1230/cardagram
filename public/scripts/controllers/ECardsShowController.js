6cce4eb476e36e03a59c41ccb89e672436e74643
LilyCole <Lilia.Cole@gmail.com>
2017-01-12 17:12:09 -0800

    hides send div after e-mail composed and removes dead code from controller

--- public/scripts/controllers/ECardsShowController.js
+++ public/scripts/controllers/ECardsShowController.js
@@ -16,34 +16,21 @@ function ECardsShowController($http, $routeParams) {
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
-  
   var getFullText = function(questions) {
     var fullTexts = [];
     questions.forEach(function(question) {
       var fullText = question.prompt;
-      fullText = fullText.replace("_____", question.response);
+      fullText = fullText.replace('_____', question.response);
       fullTexts.push(fullText);      
     })
     return fullTexts;
   }
-  vm.openModal = function(count) {
-    document.getElementById('question-modal').style.display = "block";
-    var questionId = "question-"+count;
-    document.getElementById(questionId).style.display = "block";
-  }
-  vm.closeModal = function() {
-    document.getElementById('question-modal').style.display = "none";
-    var questions = document.getElementsByClassName('questions');
-    for(var i=0; i < questions.length; i++) {
-      questions[i].style.display = 'none';
-    }
-  }
   vm.getFrontPrompt = function(question) {
-    var parts = question.prompt.split("_____");
+    var parts = question.prompt.split('_____');
     return parts[0];
   }
   vm.getResponse = function(question) {
-    if (question.response != "") {
+    if (question.response != '') {
       var placeholders = document.getElementsByClassName('placeholder-span');
       for(var i=0; i < placeholders.length; i++) {
         placeholders[i].style.display = 'none';
@@ -52,22 +39,23 @@ function ECardsShowController($http, $routeParams) {
     } else {
       var placeholders = document.getElementsByClassName('placeholder-span');
       for(var i=0; i < placeholders.length; i++) {
-        placeholders[i].style.display = "inline";
+        placeholders[i].style.display = 'inline';
       }
-      return "";
+      return '';
     }
   }
   vm.getBackPrompt = function(question) {
-    var parts = question.prompt.split("_____");
+    var parts = question.prompt.split('_____');
     return parts[1];
   }
   vm.sendECard = function() {
-    var formattedBody = "Hi " + vm.receiverName + "!\n\n " +
-      vm.senderName + " sent you a very special ecard! You can view it here: " + window.location.href + " \n\n " +
-      "Enjoy!";
-    var mailToLink = "mailto:" + vm.receiverEmail + "?subject=" + vm.ecard.theme.title +
-      "&body=" + encodeURIComponent(formattedBody);
+    var formattedBody = 'Hi ' + vm.receiverName + '!\n\n ' +
+      vm.senderName + ' sent you a very special ecard! You can view it here: ' + window.location.href + ' \n\n ' +
+      'Enjoy!';
+    var mailToLink = 'mailto:' + vm.receiverEmail + '?subject=' + vm.ecard.theme.title +
+      '&body=' + encodeURIComponent(formattedBody);
     window.location.href = mailToLink;
+    document.getElementById('send-div').style.display = 'none';
   }
 
   vm.popup = function(count) {
